2335f0ab813283ecbc278bb5fd90a9fbdcc5be2c
Nemond <koloss200@gmail.com>
2022-10-19 12:39:48 +0300

    First commit

--- src/main/java/onLesson/JUnitDemoTest.java
+++ src/main/java/onLesson/JUnitDemoTest.java
@@ -1,15 +1,41 @@
 package onLesson;
 
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class JUnitDemoTest {
+    @BeforeAll
+    static void setUp() {
+        System.out.println("### @BeforeAll");
+    }
+
+    @BeforeEach
+    void beforeEachTest() {
+        System.out.println("###         @beforeEach");
+    }
+
+    @AfterEach
+    void afterEachTest() {
+        System.out.println("###         @afterEach");
+    }
+
 
     @Test
-    void simpleTest() {
-        Assertions.assertTrue(3>2);
+    void simpleFirstTest() {
+        System.out.println("###     @firstTest");
+        Assertions.assertTrue(3 > 2);
 
+    }
+
+    @Test
+    void simpleSecondTest() {
+        System.out.println("###     @secondTest");
+        Assertions.assertTrue(3 > 1);
+    }
+
+    @AfterAll
+    static void tearDown() {
+        System.out.println("### @AfterAll");
     }
 
 }
