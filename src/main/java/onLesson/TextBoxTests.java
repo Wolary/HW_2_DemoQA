b97320072dab6de7a590dc03f71e19db5533c770
Nemond <koloss200@gmail.com>
2022-10-19 16:10:27 +0300

    Example commit

--- src/main/java/onLesson/TextBoxTests.java
+++ src/main/java/onLesson/TextBoxTests.java
@@ -40,8 +40,6 @@ public class TextBoxTests{
         $("#output #currentAddress").shouldHave(text(currentAddress));
         $("#output #permanentAddress").shouldHave(text(permanentAddress));
 
-
-
     }
 
 }
