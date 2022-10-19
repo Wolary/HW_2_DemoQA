d8d97ec0ab6900a1056d61fd30d8e0fdaca37c15
Nemond <koloss200@gmail.com>
2022-10-19 17:07:48 +0300

    HW commit

--- src/test/java/homeWork/FormTest.java
+++ src/test/java/homeWork/FormTest.java
@@ -15,7 +15,7 @@ public class FormTest {
     static void setUp() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-     //   Configuration.holdBrowserOpen = true;
+     Configuration.holdBrowserOpen = true;
     }
 
     @Test
